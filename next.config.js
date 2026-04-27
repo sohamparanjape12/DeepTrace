42cb258be4016d1a02b786f49c74604d27f3b3ce
ketak <sohamparanjape1204@gmail.com>
2026-04-28 01:56:41 +0530

    Fixed COOP Errors

--- next.config.js
+++ next.config.js
@@ -12,6 +12,19 @@ const nextConfig = {
     after: true,
   },
   serverExternalPackages: ['imghash', 'sharp', 'warcio'],
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
